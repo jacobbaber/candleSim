6c6fe45c7a7ecbfe0233dea14171396b97192ea5
Jacob Baber <jacob.baber@ou.edu>
2023-04-27 02:17:04 -0500

    Fixed candle clipping through walls and objects

--- Assets/Scripts/PickUpCandle.cs
+++ Assets/Scripts/PickUpCandle.cs
@@ -8,6 +8,12 @@ public class PickUpCandle : MonoBehaviour
     public Rigidbody rb;
     public BoxCollider coll;
     public Transform player, candleContainer, fpsCam;
+    public GameObject part1;
+    public GameObject part2;
+    public GameObject part3;
+    public GameObject lightContainer;
+    public GameObject pointLight;
+    public GameObject candleFlame;
 
     public float pickUpRange;
     public float dropFowardForce, dropUpwardForce;
@@ -54,6 +60,17 @@ public class PickUpCandle : MonoBehaviour
         transform.localRotation = Quaternion.Euler(Vector3.zero);
         transform.localScale = Vector3.one;
 
+        candle.layer = LayerMask.NameToLayer("Candle");
+
+        part1.layer = LayerMask.NameToLayer("Candle");
+        part2.layer = LayerMask.NameToLayer("Candle");
+        part3.layer = LayerMask.NameToLayer("Candle");
+        lightContainer.layer = LayerMask.NameToLayer("Candle");
+        pointLight.layer = LayerMask.NameToLayer("Candle");
+        candleFlame.layer = LayerMask.NameToLayer("Candle");
+
+        
+
 
         rb.isKinematic = true;
         coll.isTrigger = true;
@@ -69,6 +86,20 @@ public class PickUpCandle : MonoBehaviour
 
         rb.isKinematic = false;
         coll.isTrigger = false;
+
+        candle.layer = LayerMask.NameToLayer("Default");
+
+        part1.layer = LayerMask.NameToLayer("Default");
+        part2.layer = LayerMask.NameToLayer("Default");
+        part3.layer = LayerMask.NameToLayer("Default");
+        lightContainer.layer = LayerMask.NameToLayer("Default");
+        pointLight.layer = LayerMask.NameToLayer("Default");
+        candleFlame.layer = LayerMask.NameToLayer("Default");
+
+    
+        
+
+
 
 
 
